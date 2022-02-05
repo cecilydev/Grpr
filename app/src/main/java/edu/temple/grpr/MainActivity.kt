7389b5674c908649cecc3f97601da2497ff84ba5
Cecily Devaney <tul06320@temple.edu>
2022-02-04 21:01:37 -0500

    Started login fragment logic

--- app/src/main/java/edu/temple/grpr/MainActivity.kt
+++ app/src/main/java/edu/temple/grpr/MainActivity.kt
@@ -1,11 +1,48 @@
 package edu.temple.grpr
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LoginFragment.loginInterface  {
+
+    private lateinit var loginFragment : LoginFragment
+    private lateinit var registerFragment: RegisterFragment
+    private lateinit var mapFragment: MapsFragment
+    private lateinit var preferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //possibly edit
+        preferences = getSharedPreferences(LOGIN_DATA, MODE_PRIVATE)
+        val token = preferences.getString("TOKEN", null)
+
+        if(token==null){
+            loginFragment = LoginFragment()
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragmentContainerView, loginFragment)
+                .commit()
+        } else {
+            //mapFragment
+        }
+
+    }
+
+    companion object {
+        private const val LOGIN_DATA = "login_data"
+    }
+
+    override fun clickRegister() {
+        //switch fragment to register
+        registerFragment = RegisterFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainerView, registerFragment)
+            .commit()
+    }
+
+    override fun loginSuccessful() {
+        //switch fragment to map
     }
 }
