f63974b38ab0e96d928542599793d519dd04dc69
Cecily Devaney <tul06320@temple.edu>
2022-02-06 15:30:24 -0500

    added logout to menu

--- app/src/main/java/edu/temple/grpr/MainActivity.kt
+++ app/src/main/java/edu/temple/grpr/MainActivity.kt
@@ -1,8 +1,11 @@
 package edu.temple. grpr
 
+import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import edu.temple.grpr.LoginFragment
 import edu.temple.grpr.LoginFragment.*
@@ -11,40 +14,58 @@ import edu.temple.grpr.R
 
 class MainActivity : AppCompatActivity(), loginInterface {
 
-
-
     private lateinit var loginFragment : LoginFragment
     private lateinit var mapFragment: MapsFragment
-    private lateinit var preferences: SharedPreferences
+    var MAP = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
-        //possibly edit
-        preferences = getSharedPreferences(LOGIN_DATA, MODE_PRIVATE)
+        val preferences = getSharedPreferences(LOGIN_DATA, MODE_PRIVATE)
         val token = preferences.getString("TOKEN", null)
+
 
         if(token==null){
             loginFragment = LoginFragment()
             supportFragmentManager.beginTransaction()
                 .add(R.id.fragmentContainerView, loginFragment)
                 .commit()
+            MAP = false
+            invalidateOptionsMenu()
+
         } else {
             //mapFragment
             mapFragment = MapsFragment()
             supportFragmentManager.beginTransaction()
                 .add(R.id.fragmentContainerView, mapFragment)
                 .commit()
-
         }
 
     }
 
     companion object {
         private const val LOGIN_DATA = "login_data"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        val item = menu?.findItem(R.id.logout)
+        if (item != null)
+            item.setVisible(MAP)
+        return super.onPrepareOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId==R.id.logout) {
+            logout()
+            return true
+        }
+        return false
     }
 
 
@@ -54,6 +75,14 @@ class MainActivity : AppCompatActivity(), loginInterface {
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragmentContainerView, mapFragment)
             .commit()
+        MAP = true
+        invalidateOptionsMenu()
+    }
+
+    fun logout() {
+        //wipe token/login data
+        //exit app
+        finish()
     }
 
 
