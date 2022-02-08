f629a6c6d1bd6d0d3f2172fd6a4e05c13f15ff46
Cecily Devaney <tul06320@temple.edu>
2022-02-08 07:36:28 -0500

    updates to location service

--- app/src/main/java/edu/temple/grpr/MyLocationService.kt
+++ app/src/main/java/edu/temple/grpr/MyLocationService.kt
@@ -2,18 +2,21 @@ package edu.temple.grpr
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
 
-class MyLocationService : Service() {
+class LocationService : Service() {
+
+    //create notficiation for foreground service
+
+    //set up handler that communicates location
+    //create VM with LatLng?
+    inner class LocationBinder: Binder() {
+
+    }
 
     override fun onBind(intent: Intent): IBinder {
         //create notficiation for foreground service
-
-        //set up handler that communicates location
-            //create VM with LatLng?
-
-
-        //"Return the communication channel to the service."
-
+        return LocationBinder()
     }
 }
